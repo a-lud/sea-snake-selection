9a64ebf1ee5dbdb6e5d816ce4388625d9f02a7d9
a-lud <alastair.j.lud@gmail.com>
2022-12-14 11:49:10 +1030

    Addition of scripts

---
+++ ortholgs/ortholog-detection/scripts/orthologs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+## Location of where the pipeline is installed
+PIPE='/home/a1645424/al/tools_AL/nf-pipelines'
+DIR='/home/a1645424/al/analyses/orthologs'
+ASM="${DIR}/genomes"
+GFF="${DIR}/gff3"
+
+nextflow run ${PIPE}/main.nf \
+    --pipeline 'orthofinder' \
+    -profile 'conda,afw' \
+    --partition 'afw' \
+    -work-dir "${DIR}/work-dir" \
+    -N 'alastair.ludington@adelaide.edu.au' \
+    -resume \
+    --outdir "${DIR}/results" \
+    --out_prefix "orthologs" \
+    --gffs "${GFF}" \
+    --genomes "${ASM}" \
+    --search_prog 'mmseqs'
