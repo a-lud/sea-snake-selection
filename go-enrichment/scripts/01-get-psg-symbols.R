7a02358039e7b329cb40b6c317a0ae6e51871fe5
a-lud <alastair.j.lud@gmail.com>
2023-02-28 15:27:10 +1030

    Update: Better approach to enrichment testing

---
+++ go-enrichment/scripts/01-get-psg-symbols.R
@@ -0,0 +1,61 @@
+# ------------------------------------------------------------------------------------------------ #
+# Create gene list for PANTHER
+#
+# This script generates the gene list that was passed to PANTHER. Most single-copy orthologs were
+# able to be assigned one gene symbol. In instances where there are multiple, this script aims to
+#     1) Determine if the 'extra' gene symbols are just locus tages (e.g. CUNH...)
+#     2) If there are genuinely two symbols, choose one
+
+# ------------------------------------------------------------------------------------------------ #
+# Libraries
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(here)
+})
+
+# ------------------------------------------------------------------------------------------------ #
+# PSGs
+orthogroups <- read_csv(
+  file = here('orthologs','ortholog-annotation','results','ortholog-annotation','orthologs-13.csv'),
+  col_names = TRUE,
+  col_types = cols()
+)
+
+psgs <- read_lines(file = here('selection','results-13','results-PSGs','PSGs-marine.txt'))
+
+# 99 PSGs with no gene symbol (83 with NA, 16 with locus tags (CUNH.../C...H...ORF...))
+orthogroups |>
+  filter(orthogroup %in% psgs, is.na(symbol)) |>
+  nrow()
+
+orthogroups |>
+  filter(orthogroup %in% psgs, !is.na(symbol), str_detect(symbol, ' ')) |>
+  mutate(
+    symbol = sub('CUNH.+ | CUNH.+', '', symbol),
+    symbol = sub('C\\d+H\\d+ORF\\d+|CZH\\d+ORF\\d+|C\\d+HXORF\\d+', '', symbol)
+  ) |>
+  filter(symbol == '') |>
+  nrow()
+
+# 18 PSGs with multiple symbols
+ids.multiple <- orthogroups |>
+  filter(orthogroup %in% psgs, !is.na(symbol), str_detect(symbol, ' ')) |>
+  mutate(
+    symbol = sub('CUNH.+ | CUNH.+', '', symbol),
+    symbol = sub('C\\d+H\\d+ORF\\d+|CZH\\d+ORF\\d+|C\\d+HXORF\\d+', '', symbol)
+  ) |>
+  filter(symbol != '') |>
+  pull(symbol) |>
+  str_split(' ') |>
+  unlist()
+
+# 1,273 genes with a single symbol
+ids.genes <- orthogroups |>
+  filter(orthogroup %in% psgs, !is.na(symbol), !str_detect(symbol, ' ')) |>
+  pull(symbol)
+
+# ------------------------------------------------------------------------------------------------ #
+# Write gene symbols to file for use in PANTHER
+c(ids.genes, ids.multiple) |>
+  write_lines(file = here('go-enrichment','results-13','PSG-gene-symbols-for-PANTHER.txt'))
+
---
+++ go-enrichment/scripts/02-parse-panther.R
@@ -0,0 +1,101 @@
+# ------------------------------------------------------------------------------------------------ #
+# Process PANTHER
+#
+# Parses the PANTHER JSON files to obtain a hierarchical table of significant GO terms.
+
+# ------------------------------------------------------------------------------------------------ #
+# Libraries
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(here)
+})
+
+# ------------------------------------------------------------------------------------------------ #
+# PANTHER JSON parser
+parsePanther <- function(path) {
+  # Read JSON files
+  jsons <- fs::dir_ls(
+    path = path,
+    glob = '*.json'
+  ) %>%
+    set_names(sub('*\\..*', '\\1', basename(.))) |>
+    map(jsonlite::read_json)
+
+  jsons |>
+    map(\(json) {
+      # Data is hierachical (most interesting term first). Get the 'group' field
+      # with interesting data
+      group <- json |>
+        pluck(1,'group')
+
+      # Iterate over each 'result' field. The results are nested under the top level
+      map(group, \(iter) {
+
+        # This will be 'NULL' if there are multiple nested terms below the top term
+        lvl <- iter |> pluck('result', 'term')
+        if (is.null(lvl)) {
+
+          # Multiple sub-levels - need to iterate over each
+          iter |>
+            pluck('result') |>
+            map(\(iter2) {
+              tibble(
+                'level' = iter2$term$level,
+                'GO' = iter2$term$id,
+                'label' = iter2$term$label,
+                'Total' = iter2$number_in_reference,
+                'Expected' = iter2$input_list$expected,
+                'Observed' = iter2$input_list$number_in_list,
+                'Fold enrichment' = iter2$input_list$fold_enrichment,
+                'FDR' = iter2$input_list$fdr,
+                'P-value' = iter2$input_list$pValue,
+                'Direction' = iter2$plus_minus,
+                'Genes' = paste(unlist(iter2$input_list$mapped_id_list$mapped_id), collapse = ' ')
+              )
+            }) |>
+            list_rbind()
+        } else {
+          # Only one term - make the dataframe from the results
+          result <- iter |> pluck('result')
+          tibble(
+            'level' = result$term$level,
+            'GO' = result$term$id,
+            'label' = result$term$label,
+            'Total' = result$number_in_reference,
+            'Expected' = result$input_list$expected,
+            'Observed' = result$input_list$number_in_list,
+            'Fold enrichment' = result$input_list$fold_enrichment,
+            'P-value' = result$input_list$pValue,
+            'FDR' = result$input_list$fdr,
+            'Direction' = result$plus_minus,
+            'Genes' = paste(unlist(result$input_list$mapped_id_list$mapped_id), collapse = ' ')
+          )
+        }
+      }) |>
+        list_rbind()
+    }) |>
+    list_rbind(names_to = 'Ontology') |>
+    filter(label != 'UNCLASSIFIED') |>
+    select(
+      Ontology, level, GO, label, everything()
+    ) |>
+    mutate(Ontology = str_remove(Ontology, 'overrepresentation-panther-'))
+}
+
+# ------------------------------------------------------------------------------------------------ #
+# Parse results
+sig.panther <- parsePanther(here('go-enrichment', 'results-13', 'panther'))
+
+# ------------------------------------------------------------------------------------------------ #
+# Export GO terms for REVIGO and a table for supp. material
+sig.panther |>
+  pull(GO) |>
+  write_lines(file = here('go-enrichment', 'results-13', 'panther', 'enriched-GO-terms-for-REVIGO.txt'))
+
+# Write to supplementary table
+sig.panther |>
+  write_csv(
+    file = here('figures', 'supplementary', 'table-x-enrichment-panther.csv'),
+    col_names = TRUE
+  )
+
