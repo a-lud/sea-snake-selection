7ead22593802a96637f0f2b123fb71c1cc768878
a-lud <alastair.j.lud@gmail.com>
2023-06-18 09:10:12 +0930

    Removed sample that wasn't included

--- assembly/hydrophis_elegans/scripts/02-assembly_assessment/03-merqury-nextpolish.sh
+++ assembly/hydrophis_elegans/scripts/02-assembly_assessment/03-merqury-nextpolish.sh
@@ -2,33 +2,12 @@
 
 DIR='/home/a1645424/al/garvin'
 READS='/home/a1645424/al/sagc-sequence-data/merged'
-
-AIPY="${DIR}/nextpolish/aipysurus_laevis/aipysurus_laevis.polished.fa"
 ELEG="${DIR}/nextpolish/hydrophis_elegans/hydrophis_elegans.polished.fa"
-
 OUT="${DIR}/genome-assessment-polished"
-
-mkdir -p "${OUT}/aipysurus_laevis"
-cd "${OUT}/aipysurus_laevis" || exit 1
 
 CONDA_BASE=$(conda info --base)
 source "${CONDA_BASE}/etc/profile.d/conda.sh"
 conda activate merqury
-
-# Create Meryl db
-meryl count \
-    k=21 \
-    threads=8 \
-    memory=100 \
-    "${READS}/Aipysurus_laevis-KLS1140_R1.fastq.gz" \
-    "${READS}/Aipysurus_laevis-KLS1140_R2.fastq.gz" \
-    output Aipysurus_laevis-reads.meryl
-
-# Compare genome to reads
-merqury.sh \
-    Aipysurus_laevis-reads.meryl \
-    "${AIPY}" \
-    Aipysurus_laevis-to-reads
 
 # Hydrophis elegans turn
 mkdir -p "${OUT}/hydrophis_elegans"
