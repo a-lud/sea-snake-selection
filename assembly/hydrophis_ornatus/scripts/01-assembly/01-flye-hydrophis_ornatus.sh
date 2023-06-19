9d6e16fd1bf7a08b12ed7571e34e60557eca2a7e
jillianhammond <36130301+jillianhammond@users.noreply.github.com>
2023-06-19 16:34:06 +1000

    Update 01-flye-hydrophis_ornatus.sh

--- assembly/hydrophis_ornatus/scripts/01-assembly/01-flye-hydrophis_ornatus.sh
+++ assembly/hydrophis_ornatus/scripts/01-assembly/01-flye-hydrophis_ornatus.sh
@@ -1,10 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#$ -S /bin/bash
 
-DIR=$(pwd)
-OUT="${DIR}/Flye"
+#arg1: path to ONT reads
+#arg2: path to output directory
+READS=${1}
+FLYE_OUT=${2}
+THREADS=32
+GENOME_SIZE=2.0g
 
-flye \
-  --nano-hq "${SEQ}/hydrophis_ornatus.fastq.gz" \
-  --out-dir "${OUT}/hydrophis_ornatus" \
-  --threads 30 \
-  -i 2
+/usr/bin/time -v flye --threads ${THREADS} -g ${GENOME_SIZE} --nano-raw ${READS} --iterations 2 --out-dir ${FLYE_OUT} --trestle
+
