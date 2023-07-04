3498365eff762c343e7e4c3ea20888e1e60daf2f
jillianhammond <36130301+jillianhammond@users.noreply.github.com>
2023-07-04 11:30:42 +1000

    Update 03.1-purge_hap-hydrophis_ornatus.sh

--- assembly/hydrophis_ornatus/scripts/01-assembly/03.1-purge_hap-hydrophis_ornatus.sh
+++ assembly/hydrophis_ornatus/scripts/01-assembly/03.1-purge_hap-hydrophis_ornatus.sh
@@ -11,11 +11,3 @@ minimap2 -t 32 --MD -ax map-ont --secondary=no $REF $READS > ${1}/${4}.sam
   samtools sort -@ 32 ${1}/${4}.sam -o ${1}/${4}.bam
   samtools index ${1}/${4}.bam
 
-# Step 2: Coverage histogram
-purge_haplotigs hist -t 30 -b ${1}/${4}.bam -g "${ASM}"
-
-# Step 3: Contig coverage stats (mark suspect contigs)
-purge_haplotigs cov -i aligned.bam.genecov -l ... -m ... -h ... -o "${OUT}/coverage_stats.csv"
-
-# Step 4: Purge the haplotigs
-purge_haplotigs purge -g "${ASM}" -c "${OUT}/coverage_stats.csv" 
